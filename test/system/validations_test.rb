a81bdd38b531386f9079b1e9eeef8d436116768e
jeandgardany <jeandgardany@hotmail.com>
2018-10-01 20:05:59 -0300

    complemento

---
+++ test/system/validations_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class ValidationsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit validations_url
+  #
+  #   assert_selector "h1", text: "Validation"
+  # end
+end
